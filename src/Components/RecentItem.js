8ff981a55506c1c48802d528e568ddcadf06c46c
Jimmy Chan <jchan85@gatech.edu>
2022-08-07 01:22:58 -0700

    Fixed video frames

--- src/Components/RecentItem.js
+++ src/Components/RecentItem.js
@@ -4,15 +4,17 @@ import "./RecentItem.css";
 const RecentItem = ({ url, title }) => {
   return (
     <div className="item">
-      <iframe
-        width="400"
-        height="175"
-        src={url}
-        //title="Learn the MERN Stack - Full Tutorial for Beginners (MongoDB, Express, React, NodeJS) in 12Hrs (2021)"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+      <div className="video">
+        <iframe
+          // width="400"
+          // height="175"
+          src={url}
+          //title="Learn the MERN Stack - Full Tutorial for Beginners (MongoDB, Express, React, NodeJS) in 12Hrs (2021)"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
       <div className="item__details">
         <img src="https://static-cdn.jtvnw.net/ttv-boxart/Science%20&%20Technology-285x380.jpg"></img>
         <h4>{title}</h4>
